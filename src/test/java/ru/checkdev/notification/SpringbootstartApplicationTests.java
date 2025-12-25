5c0b2934e79c45f6ec8744b56ac9e7a9cc752a03
KatUser <ekaterina.shirokikh@gmail.com>
2025-12-25 11:31:15 +0700

    Fixing test runs

--- src/test/java/ru/checkdev/notification/SpringbootstartApplicationTests.java
+++ src/test/java/ru/checkdev/notification/SpringbootstartApplicationTests.java
@@ -1,11 +1,13 @@
 package ru.checkdev.notification;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @TestPropertySource("classpath:application-test.properties")
+@AutoConfigureMockMvc
 class SpringbootstartApplicationTests {
 
     @Test
--- src/test/java/ru/checkdev/notification/web/SubscribeCategoriesControllerTest.java
+++ src/test/java/ru/checkdev/notification/web/SubscribeCategoriesControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@AutoConfigureMockMvc
 public class SubscribeCategoriesControllerTest {
 
     @Autowired
